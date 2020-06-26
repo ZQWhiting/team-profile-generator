3bef5bd8bdbe0466aaef5aa020c13e7663725e97
ZQWhiting <zach.whiting@icloud.com>
2020-06-26 15:55:06 -0600

    created Engineer class

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('../lib/Engineer');
+
+test('Creates an Engineer object', () => {
+    const engineer = new Engineer('name', '53662', 'example@email.com', 'myGithub');
+
+    expect(engineer.name).toBe('name')
+    expect(engineer.id).toBe('53662')
+    expect(engineer.email).toBe('example@email.com')
+    expect(engineer.github).toBe('myGithub')
+})
+
+test(`gets engineer's github`, () => {
+    const engineer = new Engineer('name', '53662', 'example@email.com', 'myGithub');
+
+    expect(engineer.getGithub()).toBe(engineer.github);
+})
+
+test(`gets engineer's role`, () => {
+    const engineer = new Engineer('name', '53662', 'example@email.com', 'myGithub');
+
+    expect(engineer.getRole()).toBe('Engineer');
+})
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,7 +3,10 @@ const Manager = require('../lib/Manager');
 test('creates manager object', () => {
     const manager = new Manager('name', '53662', 'example@email.com', '23-B');
 
-    expect(manager.officeNumber).toBe('23-B');
+    expect(manager.name).toBe('name')
+    expect(manager.id).toBe('53662')
+    expect(manager.email).toBe('example@email.com')
+    expect(manager.officeNumber).toBe('23-B')
 })
 
 test(`gets manager's role`, () => {
