ea0639c7cdeba3ecf7fe3fa91a33e0fd591a93fa
ZQWhiting <zach.whiting@icloud.com>
2020-06-28 21:06:18 -0600

    combined Team class methods into Team.buildTeam()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Team = require('./lib/Team')
 const team = new Team();
 
 (async () => {
-    await team.startBuild();
+    await team.buildTeam(team);
     console.log(team);
 
     // rest of code goes here.
