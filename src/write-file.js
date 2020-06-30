2bc4539f9cd47edd34ec013d9e48f6a978e9b282
ZQWhiting <zach.whiting@icloud.com>
2020-06-30 10:56:49 -0600

    write file to unique path

--- src/write-file.js
+++ src/write-file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // function to write HTML file
 function writeFile(html) {
     return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/HTML.html', html, err => {
+        fs.writeFile(getPath(), html, err => {
             if (err) {
                 reject(err);
                 return;
@@ -15,6 +15,20 @@ function writeFile(html) {
             });
         });
     });
+}
+
+// Get unique file path.
+function getPath() {
+    let i = 1
+    let repeat = true;
+    while (repeat) {
+        if (fs.existsSync(`./dist/Team-${i}.html`)) {
+            i++
+        }
+        else {
+            return `./dist/Team-${i}.html`
+        }
+    }
 }
 
 module.exports = writeFile;
